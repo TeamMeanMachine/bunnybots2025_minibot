a0b0419a686f69e3424fac2dd97263f00514fa61
TMMShared <camasinfiniteresource@gmail.com>
2025-11-13 20:59:37 -0800

    Added pose estimation

--- src/main/kotlin/frc/team2471/bunnyBots2025_Minibot/Drive.kt
+++ src/main/kotlin/frc/team2471/bunnyBots2025_Minibot/Drive.kt
@@ -1,17 +1,50 @@
 package frc.team2471.bunnyBots2025_Minibot
 
 import com.ctre.phoenix6.hardware.TalonFX
+import com.studica.frc.AHRS
+import edu.wpi.first.math.VecBuilder
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator
+import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics
+import edu.wpi.first.wpilibj.SPI
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import org.littletonrobotics.junction.AutoLogOutput
 import org.team2471.frc.lib.ctre.applyConfiguration
 import org.team2471.frc.lib.ctre.brakeMode
 import org.team2471.frc.lib.ctre.currentLimits
 import org.team2471.frc.lib.ctre.inverted
 import org.team2471.frc.lib.math.deadband
+import org.team2471.frc.lib.units.asMeters
+import org.team2471.frc.lib.units.asRadians
+import org.team2471.frc.lib.units.asRotation2d
+import org.team2471.frc.lib.units.degrees
+import org.team2471.frc.lib.units.inches
+import org.team2471.frc.lib.units.wrap
 import kotlin.math.abs
 
 object Drive: SubsystemBase("Drive") {
+
+
+
     val leftMotor = TalonFX(Falcons.LEFT_DRIVE)
     val rightMotor = TalonFX(Falcons.RIGHT_DRIVE)
+
+    private val navx = AHRS(AHRS.NavXComType.kMXP_SPI)
+    @get:AutoLogOutput(key = "Drive/Heading")
+    val heading get() = navx.angle.degrees
+
+    @get:AutoLogOutput(key = "Drive/Pose")
+    var pose = Pose2d()
+
+    @get:AutoLogOutput(key = "Drive/Distance Right")
+    var distRight = 0.0
+    @get:AutoLogOutput(key = "Drive/Distance Left")
+    var distLeft = 0.0
+
+    val wheelRadius = (1.0 + 15.0/16.0).inches
+    val kinematics = DifferentialDriveKinematics(17.0.inches)
+    val poseEstimator = DifferentialDrivePoseEstimator(kinematics, Rotation2d(), 0.0, 0.0, Pose2d(), VecBuilder.fill(0.5, 0.5, 0.5), VecBuilder.fill(0.01, 0.01, 0.01))
 
     init {
         leftMotor.applyConfiguration {
@@ -35,6 +68,7 @@ object Drive: SubsystemBase("Drive") {
 
     override fun periodic() {
         joystickDrive()
+        updateOdometry()
     }
 
     fun joystickDrive() {
@@ -48,6 +82,14 @@ object Drive: SubsystemBase("Drive") {
 
         leftMotor.setVoltage(leftPower * 12.0)
         rightMotor.setVoltage(rightPower * 12.0)
+    }
+
+    fun updateOdometry() {
+        val distanceRight = rightMotor.position.value.asRadians * wheelRadius.asMeters / 5.8
+        val distanceLeft = leftMotor.position.value.asRadians * wheelRadius.asMeters / 5.8
+        distLeft = distanceLeft
+        distRight = distanceRight
+        pose = poseEstimator.update(heading.asRotation2d, distanceLeft, distanceRight)
     }
 
 }
--- src/main/kotlin/frc/team2471/bunnyBots2025_Minibot/Vision.kt
+++ src/main/kotlin/frc/team2471/bunnyBots2025_Minibot/Vision.kt
@@ -1,11 +1,15 @@
 package frc.team2471.bunnyBots2025_Minibot
 
+import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.team2471.bunnyBots2025_Minibot.Drive.poseEstimator
 import org.team2471.frc.lib.vision.limelight.VisionIO
+import org.team2471.frc.lib.vision.limelight.VisionIOLimelight
 
 // Single limelight 3g
 object Vision : SubsystemBase() {
     val io: Array<VisionIO> = arrayOf(
+        VisionIOLimelight("limelight-shooter", false, {Drive.heading})
     )
     
     val inputs = Array(io.size) { VisionIO.VisionIOInputs() }
@@ -13,6 +17,9 @@ object Vision : SubsystemBase() {
     override fun periodic() {
         for (i in io.indices) {
             io[i].updateInputs(inputs[i])
+            if (inputs[i].aprilTagPoseEstimate != Pose2d()) {
+                poseEstimator.addVisionMeasurement(inputs[i].aprilTagPoseEstimate, inputs[i].aprilTagTimestamp)
+            }
         }
     }
 
